c6b348c6c40a545c5ddab2cf866b76ef61e2fade
Ankit Bharvad <36946135+Ankit2005@users.noreply.github.com>
2023-11-19 18:36:22 +0530

    add express js

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
+import app from "./app";
 import { Config } from "./config";
 
-function login(userName: string): string {
-    // eslint-disable-next-line no-console
-    console.log("APP IS RUNNING ON PORT : ", Config.PORT);
-    return userName;
-}
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => {
+            // eslint-disable-next-line no-console
+            console.log(`Listening on port ${PORT}`);
+        });
+    } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+        process.exit(1);
+    }
+};
 
-login("ankit");
+startServer();
