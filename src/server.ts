975671c98bcf56268532d4bc25f894f1eedcf247
Ankit Bharvad <36946135+Ankit2005@users.noreply.github.com>
2023-11-19 18:00:48 +0530

    add config setup

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import { Config } from "./config";
+
 function login(userName: string): string {
-    // console.log(userName);
+    // eslint-disable-next-line no-console
+    console.log("APP IS RUNNING ON PORT : ", Config.PORT);
     return userName;
 }
 
