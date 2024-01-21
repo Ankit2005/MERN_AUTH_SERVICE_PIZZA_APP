ab8863ed09e9872ce2b56327fab426a6e4d09aff
Ankit2005 <ankitmb15@gmail.com>
2024-01-21 23:06:16 +0530

    add test for persist user

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,15 +1,34 @@
 import request from "supertest";
 import app from "../../src/app";
+import { DataSource } from "typeorm";
+import { User } from "../../src/entity/User";
+import { AppDataSource } from "../../src/config/data-source";
+import { truncateTables } from "../utils";
 
 describe("POST /aut/register", () => {
+    let connection: DataSource;
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    beforeEach(async () => {
+        // Database truncate
+        await truncateTables(connection);
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     describe("Given all fields", () => {
         it("should return the 201 status code", async () => {
             // Arrange
             const userData = {
-                firtName: "ankit",
+                firstName: "ankit",
                 lastName: "bharvad",
                 email: "ankitmb15@gmail.com",
-                password: "1234",
+                password: "ankit",
             };
 
             // Act
@@ -24,10 +43,10 @@ describe("POST /aut/register", () => {
         it("should return valid json response", async () => {
             // Arrange
             const userData = {
-                firtName: "ankit",
+                firstName: "ankit",
                 lastName: "bharvad",
                 email: "ankitmb15@gmail.com",
-                password: "1234",
+                password: "ankit",
             };
 
             // Act
@@ -44,16 +63,24 @@ describe("POST /aut/register", () => {
         it("should persist the user in the database", async () => {
             // Arrange
             const userData = {
-                firtName: "ankit",
+                firstName: "ankit",
                 lastName: "bharvad",
                 email: "ankitmb15@gmail.com",
-                password: "1234",
+                password: "ankit",
             };
 
             // Act
             await request(app).post("/auth/register").send(userData);
 
             // Assert
+
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            // console.log("users -->", await users);
+            expect(users).toHaveLength(1);
+            expect(users[0].firstName).toBe(userData.firstName);
+            expect(users[0].lastName).toBe(userData.lastName);
+            expect(users[0].email).toBe(userData.email);
         });
     });
 
