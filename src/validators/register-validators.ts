351771d8ba2c53792cb59fe3365f728ea391f1f6
Ankit2005 <ankitmb15@gmail.com>
2024-04-19 23:54:06 +0530

    fix

--- src/validators/register-validators.ts
+++ src/validators/register-validators.ts
@@ -24,3 +24,25 @@ export default checkSchema({
         },
     },
 });
+
+// DOCKERHUB_PASSWORD = dckr_pat_qphkl1nY2rkOGLHWby3TbPh1oNE
+
+// DOCKERHUB_USERNAME = ankitbharvad
+
+// JWKS_URI = http://localhost:5501/.well-known/jwks.json
+
+// PRIVATE_KEY
+
+// REFRESH_TOKEN_SECRET = SECRETKEYGOESHERE
+
+// SONAR_TOKEN = 12acdfbb9c6acf93bf918686f8fbc04279fbee21
+
+// DB_HOST = aws-0-ap-southeast-1.pooler.supabase.com
+
+// DB_NAME = postgres
+
+// DB_PASSWORD = Ankit2@@5..
+
+// DB_PORT = 5432
+
+// DB_USERNAME = postgres.dtjmnkdfdduyvsiebbba
