795f6dec9385087da96b2dba87b3e176f9207c73
Ankit2005 <ankitmb15@gmail.com>
2024-04-21 16:44:58 +0530

    remove test case in comment

--- tests/users/create.spec.ts
+++ tests/users/create.spec.ts
@@ -33,81 +33,98 @@ describe("POST /users", () => {
     });
 
     describe("Given all fields", () => {
-        // it("should persist the user in the database", async () => {
-        //     // Create tenant first
-        //     const tenant = await createTenant(connection.getRepository(Tenant));
-        //     const adminToken = jwks.token({
-        //         sub: "1",
-        //         role: Roles.ADMIN,
-        //     });
-        //     // Register user
-        //     const userData = {
-        //         firstName: "Rakesh",
-        //         lastName: "K",
-        //         email: "rakesh@mern.space",
-        //         password: "password",
-        //         tenantId: tenant.id,
-        //         role: Roles.MANAGER,
-        //     };
-        //     // Add token to cookie
-        //     await request(app)
-        //         .post("/users")
-        //         .set("Cookie", [`accessToken=${adminToken}`])
-        //         .send(userData);
-        //     const userRepository = connection.getRepository(User);
-        //     const users = await userRepository.find();
-        //     expect(users).toHaveLength(1);
-        //     expect(users[0].email).toBe(userData.email);
-        // });
-        // it("should create a manager user", async () => {
-        //     // Create tenant
-        //     const tenant = await createTenant(connection.getRepository(Tenant));
-        //     const adminToken = jwks.token({
-        //         sub: "1",
-        //         role: Roles.ADMIN,
-        //     });
-        //     // Register user
-        //     const userData = {
-        //         firstName: "Rakesh",
-        //         lastName: "K",
-        //         email: "rakesh@mern.space",
-        //         password: "password",
-        //         tenantId: tenant.id,
-        //         role: Roles.MANAGER,
-        //     };
-        //     // Add token to cookie
-        //     await request(app)
-        //         .post("/users")
-        //         .set("Cookie", [`accessToken=${adminToken}`])
-        //         .send(userData);
-        //     const userRepository = connection.getRepository(User);
-        //     const users = await userRepository.find();
-        //     expect(users).toHaveLength(1);
-        //     expect(users[0].role).toBe(Roles.MANAGER);
-        // });
-        // it("should return 403 if non admin user tries to create a user", async () => {
-        //     // Create tenant first
-        //     const tenant = await createTenant(connection.getRepository(Tenant));
-        //     const nonAdminToken = jwks.token({
-        //         sub: "1",
-        //         role: Roles.MANAGER,
-        //     });
-        //     const userData = {
-        //         firstName: "Rakesh",
-        //         lastName: "K",
-        //         email: "rakesh@mern.space",
-        //         password: "password",
-        //         tenantId: tenant.id,
-        //     };
-        //     // Add token to cookie
-        //     const response = await request(app)
-        //         .post("/users")
-        //         .set("Cookie", [`accessToken=${nonAdminToken}`])
-        //         .send(userData);
-        //     expect(response.statusCode).toBe(403);
-        //     const userRepository = connection.getRepository(User);
-        //     const users = await userRepository.find();
-        //     expect(users).toHaveLength(0);
-        // });
+        it("should persist the user in the database", async () => {
+            // Create tenant first
+            const tenant = await createTenant(connection.getRepository(Tenant));
+
+            const adminToken = jwks.token({
+                sub: "1",
+                role: Roles.ADMIN,
+            });
+
+            // Register user
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "password",
+                tenantId: tenant.id,
+                role: Roles.MANAGER,
+            };
+
+            // Add token to cookie
+            await request(app)
+                .post("/users")
+                .set("Cookie", [`accessToken=${adminToken}`])
+                .send(userData);
+
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+
+            expect(users).toHaveLength(1);
+            expect(users[0].email).toBe(userData.email);
+        });
+
+        it("should create a manager user", async () => {
+            // Create tenant
+            const tenant = await createTenant(connection.getRepository(Tenant));
+
+            const adminToken = jwks.token({
+                sub: "1",
+                role: Roles.ADMIN,
+            });
+
+            // Register user
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "password",
+                tenantId: tenant.id,
+                role: Roles.MANAGER,
+            };
+
+            // Add token to cookie
+            await request(app)
+                .post("/users")
+                .set("Cookie", [`accessToken=${adminToken}`])
+                .send(userData);
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+
+            expect(users).toHaveLength(1);
+            expect(users[0].role).toBe(Roles.MANAGER);
+        });
+
+        it("should return 403 if non admin user tries to create a user", async () => {
+            // Create tenant first
+            const tenant = await createTenant(connection.getRepository(Tenant));
+
+            const nonAdminToken = jwks.token({
+                sub: "1",
+                role: Roles.MANAGER,
+            });
+
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "password",
+                tenantId: tenant.id,
+            };
+
+            // Add token to cookie
+            const response = await request(app)
+                .post("/users")
+                .set("Cookie", [`accessToken=${nonAdminToken}`])
+                .send(userData);
+
+            expect(response.statusCode).toBe(403);
+
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+
+            expect(users).toHaveLength(0);
+        });
     });
 });
